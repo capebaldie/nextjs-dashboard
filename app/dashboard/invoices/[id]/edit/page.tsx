fd7a724cd369be66a77db837632c269cf3462340
amal a s <amalsunil863@gmail.com>
2025-05-07 15:48:12 +0530

    added error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -12,6 +13,9 @@ export default async function Page({
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  if (!invoice) {
+    return notFound();
+  }
   return (
     <main>
       <Breadcrumbs
--- app/lib/action.ts
+++ app/lib/action.ts
@@ -25,10 +25,14 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = parsedData.amount * 100;
   const date = new Date().toISOString().split("T")[0];
-  await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${parsedData.customerId}, ${amountInCents}, ${parsedData.status}, ${date})
-    `;
+  try {
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${parsedData.customerId}, ${amountInCents}, ${parsedData.status}, ${date})
+      `;
+  } catch (error) {
+    console.log(error);
+  }
   // refetching the page for fresh data otherwise it'll use cached one
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -43,16 +47,24 @@ export async function editInvoice(id: string, formData: FormData) {
   });
   const amountInCents = parsedData.amount * 100;
   const date = new Date().toISOString().split("T")[0];
-  await sql`
-    UPDATE invoices  
-    SET customer_id = ${parsedData.customerId}, amount = ${amountInCents}, status = ${parsedData.status}, date = ${date}
-    WHERE id = ${id}`;
+  try {
+    await sql`
+      UPDATE invoices  
+      SET customer_id = ${parsedData.customerId}, amount = ${amountInCents}, status = ${parsedData.status}, date = ${date}
+      WHERE id = ${id}`;
+  } catch (error) {
+    console.log(error);
+  }
   // refetching the page for fresh data otherwise it'll use cached one
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.log(error);
+  }
   revalidatePath("/dashboard/invoices");
 }
